575ac75f211c65348e6f6dbdd8974023f67e9625
Gahee Kim <gahee_kim@sfu.ca>
2025-01-04 00:51:56 -0800

    Update structure.ts

--- wics-app2/sanity/structure.ts
+++ wics-app2/sanity/structure.ts
@@ -1,7 +1,20 @@
-import type {StructureResolver} from 'sanity/structure'
+import type {StructureResolver} from 'sanity/structure';
+import { orderableDocumentListDeskItem } from "@sanity/orderable-document-list";
+
+// THIS CONTROLS THE SANITY STUDIO LAYOUT
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure: StructureResolver = (S, context) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('SFU WiCS Studio')
+    .items([
+	    S.documentTypeListItem('post').title('Posts'),
+      S.documentTypeListItem('tag').title('Tags'),
+
+      orderableDocumentListDeskItem({
+        title: 'Profiles',
+        type: 'profile',
+        S,
+        context,
+      }),
+	  ]);
