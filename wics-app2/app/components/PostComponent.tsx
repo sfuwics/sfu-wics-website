191ed98a081113eb018b2d244da404810aa4a163
Gahee Kim <gahee_kim@sfu.ca>
2025-02-19 19:54:54 -0800

    Update Postcomponents

---
+++ wics-app2/app/components/PostComponent.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { Post } from "../../utils/Interface";
+import { PortableText } from "next-sanity";
+import { RichTextComponents } from "@/app/components/blog/RichTextComponents";
+
+
+interface Props {
+  posts: Post;
+}
+
+const PostComponent = ({ post }: Props) => {
+  
+  return (
+    <div className="border-t border-black py-5 sm:py-8 flex gap-1 sm:gap-5 lg:gap-10 items-start">
+      <div>
+          <h2 className="text-2xl font-medium">{post?.title}</h2>
+          <p className="pb-4 text-wics-blue-500 text-sm">
+            {new Date(post?.publishedAt).toDateString()}
+          </p>
+
+          <div className={richTextStyles}>
+            <PortableText value={post?.body}  components={RichTextComponents} />
+            
+            <PortableText value={post?.body} />
+          </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default PostComponent;
+
+const richTextStyles = `
+text-left
+text-sm
+
+m-auto
+prose-headings:my-3
+prose-heading:text-2xl
+prose-p:mb-4
+prose-p:leading-5
+prose-li:list-disc
+prose-li:leading-5
+prose-li:ml-12
+prose-ul:pb-4
+prose-ol:pb-
+`;
