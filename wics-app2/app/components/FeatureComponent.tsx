dc867c3cd525f94036ffc30919f3d0f2c6975a60
Gahee Kim <gahee_kim@sfu.ca>
2025-03-21 03:13:40 -0700

    Make gradient lighter

--- wics-app2/app/components/FeatureComponent.tsx
+++ wics-app2/app/components/FeatureComponent.tsx
@@ -18,18 +18,18 @@ const FeatureComponent: React.FC<FeatureComponentProps> = ({
   useEffect(() => {
     setIsVisible(true);
   }, []);
-  
+
   return (
-    <div className={`relative w-full h-full transition-opacity duration-700 ${isVisible ? "opacity-100" : "opacity-0"} text-white hover:text-wics-blue-500 transform transition-transform duration-300`}>
-      <div className="absolute inset-0 bg-gradient-to-b from-black/100 hover:from-white/70 to-transparent z-20 transition-opacity duration-300" /> 
+    <div
+      className={`relative h-full w-full transition-opacity duration-700 ${isVisible ? "opacity-100" : "opacity-0"} transform text-white transition-transform duration-300 hover:text-wics-blue-500`}
+    >
+      <div className="absolute inset-0 z-20 bg-gradient-to-b from-black/60 to-transparent transition-opacity duration-300 hover:from-white/60" />
       <Image src={imageSrc} alt={text} fill className="object-cover" />
-        <div className="absolute top-2 left-2 mx-2 my-2 xl:mx-4 text-xl lg:text-2xl max-w-2/3 z-20">
-          {text}
-        </div>
+      <div className="max-w-2/3 absolute left-2 top-2 z-20 mx-2 my-2 text-xl lg:text-2xl xl:mx-4">
+        {text}
+      </div>
       {/* </div> */}
     </div>
-
-    
   );
 };
 
