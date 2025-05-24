e097e827eba9e4190a0c095df34638964f0dc509
Gahee Kim <gahee_kim@sfu.ca>
2025-05-23 22:33:43 -0600

    Update webhook.ts

--- wics-app/netlify/functions/webhook.ts
+++ wics-app/netlify/functions/webhook.ts
@@ -1,22 +1,39 @@
-const fetch = require('node-fetch');
+import type { Handler } from '@netlify/functions';
 
-exports.handler = async function (event) {
+const handler: Handler = async (event) => {
   const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-  const res = await fetch('https://api.github.com/repos/sfuwics/sfu-wics-website/dispatches', {
-    method: 'POST',
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github.everest-preview+json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      event_type: 'sanity-update',
-    }),
-  });
+  if (!GITHUB_TOKEN) {
+    return {
+      statusCode: 500,
+      body: 'Missing GITHUB_TOKEN env variable',
+    };
+  }
 
-  return {
-    statusCode: res.status,
-    body: await res.text(),
-  };
+  try {
+    const response = await fetch('https://api.github.com/repos/sfuwics/sfu-wics-website/dispatches', {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github.everest-preview+json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        event_type: 'sanity-update',
+      }),
+    });
+
+    const text = await response.text();
+    return {
+      statusCode: response.status,
+      body: text,
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: `Webhook relay failed: ${error.message}`,
+    };
+  }
 };
+
+export { handler };
