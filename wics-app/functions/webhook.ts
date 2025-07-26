38f8b2c311c2d35b775d52e34ced81603f3036be
Gahee Kim <gahee_kim@sfu.ca>
2025-07-25 22:35:46 -0600

    move function out of netlify

---
+++ wics-app/functions/webhook.ts
@@ -0,0 +1,48 @@
+import type { Handler } from '@netlify/functions';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const handler: Handler = async (event) => {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+  if (!GITHUB_TOKEN) {
+    return {
+      statusCode: 500,
+      body: 'Missing GITHUB_TOKEN environment variable',
+    };
+  }
+
+  console.log('Webhook  triggered:', event.body);
+
+  // Wait 5 seconds to ensure Sanity content updates are propagated
+  await sleep(5000);
+
+  try {
+    const response = await fetch('https://api.github.com/repos/sfuwics/sfu-wics-website/dispatches', {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github.everest-preview+json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        event_type: 'sanity-update',
+      }),
+    });
+
+    const text = await response.text();
+
+    return {
+      statusCode: response.status,
+      body: text,
+    };
+  } catch (error: any) {
+    console.error('Error triggering GitHub dispatch:', error);
+    return {
+      statusCode: 500,
+      body: `Webhook relay failed: ${error.message}`,
+    };
+  }
+};
+
+export { handler };
